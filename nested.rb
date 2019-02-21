7cd9ce2c114375b61465b0cdee8abcda119d888e
roy renno <royrenno@yahoo.com>
2019-02-21 07:09:08 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -8,6 +8,7 @@ programmer_hash =
 }
 return programmer_hash[:grace_hopper]
 end
+
 def alan_kay_is_known_for
 # What combination of keys would you use to return the value of the :known_for key of :alan_kay?
 programmer_hash =
@@ -30,7 +31,7 @@ programmer_hash =
           :languages => ["C"]
         }
      }
- return programmer_hash[:dennis_ritchie][:languages] = [0],
+ return programmer_hash[:dennis_ritchie][:languages][0],
 def adding_matz
 # add the following information to the top level of programmer_hash
 # :yukihiro_matsumoto => {
@@ -41,26 +42,28 @@ programmer_hash =
  		{
         :grace_hopper => {
           :known_for => "COBOL",
+					
           :languages => ["C"]
         }
      }
 programmer_hash[:yukihiro_matsumoto] = {:known_for =>"Ruby",
 :languages=>["LISP","C"]
 }
-programmer_hash
+return programmer_hash
 end
+
 def changing_alan
 programmer_hash =
  		{
         :grace_hopper => {
           :known_for => "COBOL",
+					
           :languages => ["C"]
         }
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
-     alans_new_info = programmer_hash[:alan_kay]
- alans_new_info = alans_new_info[:known_for] = "GUI"
-   programmer_hash
+    programmer_hash[:alan_kay][:known_for]= alans_new_info
+   return programmer_hash
 end
 def adding_to_dennis
 programmer_hash =
@@ -70,6 +73,6 @@ programmer_hash =
         }
      }
 programmer_hash[:dennis_ritchie][:languages]<< "Assembly"
-programmer_hash
+return programmer_hash
 end
 end
