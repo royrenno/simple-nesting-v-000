271703b726d49878bfce2e14ff77c34a02f2e050
roy renno <royrenno@yahoo.com>
2019-02-21 06:35:15 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -68,17 +68,21 @@ programmer_hash =
         }
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
-     alans_new_info = programmer_hash[:alan_kay]
-     alans_new_info = alans_new_info[:known_for] = "GUI"
+   programmer_hash[:alan_kay][:known_for]="GUI"
+                          #alans_new_info = "GUI"
+		 
      programmer_hash
 end
+
 def adding_to_dennis
-programmer_hash =
- 		{
+     programmer_hash =
+ 		      {
         :grace_hopper => {
           :known_for => "COBOL",
         }
      }
-programmer_hash[:dennis_ritchie][:languages]<< "Assembly"
+		 
+programmer_hash [:dennis_ritchie][:languages]<< "Assembly"
+
 programmer_hash
 end
