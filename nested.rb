0c4fc156d8b0386015933656b416f47a146920fb
roy renno <royrenno@yahoo.com>
2019-02-21 07:22:29 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -10,23 +10,18 @@ def hopper
 
 
 
-}
-
-}
-
-
-return programmer_hash[:grace_hopper]
-
 end
 
 def alan_kay_is_known_for
+
+programmer_hash[grace_hopper][:known_for]
+
+end
 
 
 
 
 programmer_hash =
-
-
 
 {
 
@@ -35,14 +30,6 @@ programmer_hash =
 :known_for => "COBOL",
 
 
-
-:languages => ["C"]
-
-}
-
-}
-
-programmer_hash[:alan_kay][:known_for]
 
 end
 
@@ -55,20 +42,6 @@ programmer_hash =
 :grace_hopper => {
 
 :known_for => "COBOL",
-
-
-
-:languages => ["C"]
-
-}
-
-}
-
-programmer_hash[:dennis_ritchie][:languages].join()
-
-end
-
-def adding_matz
 
 
 
@@ -86,21 +59,9 @@ programmer_hash =
 
 
 
-:languages => ["C"]
-
 }
 
 }
-
-programmer_hash[:yukihiro_matsumoto] = {
-
-:known_for => "Ruby",
-
-:languages => ["LISP", "C"]
-
-}
-
-programmer_hash
 
 end
 
@@ -126,12 +87,6 @@ programmer_hash =
 
 alans_new_info = "GUI"
 
-
-
-programmer_hash[:alan_kay][:known_for] = alans_new_info
-
-programmer_hash
-
 end
 
 def adding_to_dennis
@@ -143,16 +98,3 @@ programmer_hash =
 :grace_hopper => {
 
 :known_for => "COBOL",
-
-
-
-}
-
-}
-
-
-programmer_hash[:dennis_ritchie][:languages] << "Assembly"
-
-programmer_hash
-
-end
