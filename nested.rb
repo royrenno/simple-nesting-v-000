8ca1161a7d1320e89d16966d7319cb06348f5d45
roy renno <royrenno@yahoo.com>
2019-02-21 07:14:23 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -3,35 +3,30 @@ programmer_hash =
  		{
         :grace_hopper => {
           :known_for => "COBOL",
-					:languages => ["COBOL","FORTRAN"]
  }
 }
-return programmer_hash[:grace_hopper]
+programmer_hash[:grace_hopper]
 end
-
 def alan_kay_is_known_for
 # What combination of keys would you use to return the value of the :known_for key of :alan_kay?
 programmer_hash =
  		{
 :grace_hopper => {
 :known_for => "COBOL",
-
           :languages => ["C"]
         }
      }
- return programmer_hash[:alan_kay][:known_for]
+ programmer_hash[:alan_kay][:known_for]
 end
-
 def dennis_ritchies_language
 programmer_hash =
  		{
         :grace_hopper => {
           :known_for => "COBOL",
-
           :languages => ["C"]
         }
      }
- return programmer_hash[:dennis_ritchie][:languages],
+ programmer_hash[:dennis_ritchie[:languages] = ["C"],
 def adding_matz
 # add the following information to the top level of programmer_hash
 # :yukihiro_matsumoto => {
@@ -42,28 +37,26 @@ programmer_hash =
  		{
         :grace_hopper => {
           :known_for => "COBOL",
-
           :languages => ["C"]
         }
      }
 programmer_hash[:yukihiro_matsumoto] = {:known_for =>"Ruby",
 :languages=>["LISP","C"]
 }
-return programmer_hash
+programmer_hash
 end
-
 def changing_alan
 programmer_hash =
  		{
         :grace_hopper => {
           :known_for => "COBOL",
-
           :languages => ["C"]
         }
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
-    programmer_hash[:alan_kay][:known_for]= alans_new_info
-   return programmer_hash
+     alans_new_info = programmer_hash[:alan_kay]
+ alans_new_info = alans_new_info[:known_for] = "GUI"
+   programmer_hash
 end
 def adding_to_dennis
 programmer_hash =
@@ -73,6 +66,5 @@ programmer_hash =
         }
      }
 programmer_hash[:dennis_ritchie][:languages]<< "Assembly"
-return programmer_hash
-end
+programmer_hash
 end
